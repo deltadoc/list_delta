37724d0928eee71ec367e6e07595cdcfa597c784
everzet <ever.zet@gmail.com>
2017-05-21 14:54:41 +0100

    Describe and implement basic construction

--- lib/list_delta.ex
+++ lib/list_delta.ex
@@ -1,18 +1,28 @@
 defmodule ListDelta do
-  @moduledoc """
-  Documentation for ListDelta.
-  """
+  defstruct ops: []
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListDelta.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defmodule Operation do
+    def insert(idx, init), do: %{insert: idx, init: init}
+    def remove(idx), do: %{remove: idx}
+    def replace(idx, new_init), do: %{replace: idx, init: new_init}
+    def change(idx, delta), do: %{change: idx, delta: delta}
   end
+
+  def insert(idx, init) do
+    %ListDelta{ops: [Operation.insert(idx, init)]}
+  end
+
+  def remove(idx) do
+    %ListDelta{ops: [Operation.remove(idx)]}
+  end
+
+  def replace(idx, new_init) do
+    %ListDelta{ops: [Operation.replace(idx, new_init)]}
+  end
+
+  def change(idx, item_delta) do
+    %ListDelta{ops: [Operation.change(idx, item_delta)]}
+  end
+
+  def operations(delta), do: delta.ops
 end
--- test/list_delta_test.exs
+++ test/list_delta_test.exs
@@ -2,7 +2,25 @@ defmodule ListDeltaTest do
   use ExUnit.Case
   doctest ListDelta
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias ListDelta.Operation
+
+  describe "construct" do
+    test "insert" do
+      assert ops(ListDelta.insert(1, nil)) == [Operation.insert(1, nil)]
+    end
+
+    test "remove" do
+      assert ops(ListDelta.remove(1)) == [Operation.remove(1)]
+    end
+
+    test "replace" do
+      assert ops(ListDelta.replace(1, 5)) == [Operation.replace(1, 5)]
+    end
+
+    test "change" do
+      assert ops(ListDelta.change(1, 3)) == [Operation.change(1, 3)]
+    end
   end
+
+  defp ops(delta), do: ListDelta.operations(delta)
 end
