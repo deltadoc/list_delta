f253f212c9d44afd29e082b2b914f91a8e75fc33
everzet <ever.zet@gmail.com>
2017-08-28 12:15:52 +0100

    Refactor OperationsIndexer

--- lib/list_delta/operations_indexer.ex
+++ lib/list_delta/operations_indexer.ex
@@ -19,16 +19,19 @@ defmodule ListDelta.OperationsIndexer do
   end
 
   defp index_op({op, _} = op_with_orig_idx, idxd_ops) do
-    idx = Operation.index(op)
-    idxd_ops
-    |> ensure_length(idx + 1)
-    |> List.replace_at(idx, op_with_orig_idx)
+    replace_at_idx(idxd_ops, Operation.index(op), op_with_orig_idx)
   end
 
   defp insert_at_idx(list, idx, val) do
     list
     |> ensure_length(idx)
     |> List.insert_at(idx, val)
+  end
+
+  defp replace_at_idx(list, idx, val) do
+    list
+    |> ensure_length(idx + 1)
+    |> List.replace_at(idx, val)
   end
 
   defp ensure_length(list, idx) when length(list) >= idx, do: list
