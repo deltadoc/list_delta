1a9a1ce2a23c3ef53d74badea58e13890d477cd0
everzet <ever.zet@gmail.com>
2017-08-28 11:22:30 +0100

    Initial implementation of operation indexer

---
+++ lib/list_delta/ops_indexer.ex
@@ -0,0 +1,28 @@
+defmodule ListDelta.OpsIndexer do
+  alias ListDelta.Operation
+
+  def index_ops(ops) do
+    ops
+    |> Enum.with_index()
+    |> List.foldl([], &index_op/2)
+  end
+
+  defp index_op({op, _} = op_with_orig_idx, idxd_ops) do
+    type = Operation.type(op)
+    idx = Operation.index(op)
+    index_op(type, idx, op_with_orig_idx, idxd_ops)
+  end
+
+  defp index_op(:insert, idx, op_with_orig_idx, idxd_ops) do
+    insert_at_idx(idxd_ops, idx, op_with_orig_idx)
+  end
+
+  defp index_op(_op_type, _idx, op_with_orig_idx, idxd_ops) do
+    idxd_ops ++ [op_with_orig_idx]
+  end
+
+  defp insert_at_idx(list, idx, val) do
+    list
+    |> List.insert_at(idx, val)
+  end
+end
