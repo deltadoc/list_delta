7592570c2e6d9c5086178ab96ff5dd401575eb5b
everzet <ever.zet@gmail.com>
2017-09-05 17:37:11 +0100

    Refactor Index a bit more

--- lib/list_delta/index.ex
+++ lib/list_delta/index.ex
@@ -3,9 +3,7 @@ defmodule ListDelta.Index do
 
   def new, do: []
 
-  def from_operations(ops) do
-    List.foldl(ops, new(), &add(&2, &1))
-  end
+  def from_operations(ops), do: List.foldl(ops, new(), &add(&2, &1))
 
   def add(ops_index, %{insert: idx} = op) do
     ops_index
@@ -19,18 +17,17 @@ defmodule ListDelta.Index do
     |> reindex_ops()
   end
   def add(ops_index, op) do
-    ops_index
-    |> replace_at_index(Operation.index(op), op)
+    replace_at_index(ops_index, Operation.index(op), op)
   end
 
-  def replace_at(ops_index, idx, op), do: List.replace_at(ops_index, idx, op)
-  def delete_at(ops_index, idx), do: List.delete_at(ops_index, idx)
+  defdelegate replace_at(ops_index, idx, op), to: List
+  defdelegate delete_at(ops_index, idx), to: List
 
   def to_operations(ops_index) do
     ops_index
     |> List.foldl([], &prepend_op(&2, &1))
-    |> Enum.reverse()
     |> Enum.filter(&(&1 != :noop))
+    |> Enum.reverse()
   end
 
   defp insert_at_index(list, idx, val) do
@@ -46,13 +43,11 @@ defmodule ListDelta.Index do
   end
 
   defp reindex_ops(ops_index) do
-    ops_index
-    |> Enum.with_index()
-    |> Enum.map(&reindex_op/1)
+    Enum.map Enum.with_index(ops_index), fn
+      {:noop, _} -> :noop
+      {op, idx} -> Operation.change_index(op, idx)
+    end
   end
-
-  defp reindex_op({:noop, _actual_idx}), do: :noop
-  defp reindex_op({op, actual_idx}), do: Operation.change_index(op, actual_idx)
 
   defp prepend_op([%{insert: prev_idx} = prev_ins | remainder],
                    %{insert: _, init: init}) do
@@ -61,7 +56,9 @@ defmodule ListDelta.Index do
   defp prepend_op([], %{insert: _, init: init}) do
     [Operation.insert(0, init)]
   end
-  defp prepend_op(ops, op), do: [op | ops]
+  defp prepend_op(ops, op) do
+    [op | ops]
+  end
 
   defp ensure_length(list, idx) when length(list) >= idx, do: list
   defp ensure_length(list, idx), do: ensure_length(list ++ [:noop], idx)
