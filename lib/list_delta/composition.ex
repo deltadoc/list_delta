35de2d63de04376e5fd6aa7fff06fbe67a047fbd
everzet <ever.zet@gmail.com>
2017-09-05 17:30:16 +0100

    Refactor Composition a bit further

--- lib/list_delta/composition.ex
+++ lib/list_delta/composition.ex
@@ -8,53 +8,56 @@ defmodule ListDelta.Composition do
       |> Index.from_operations()
     second
     |> ListDelta.operations()
-    |> List.foldl(index, &do_compose/2)
+    |> List.foldl(index, &compose_op_with_index/2)
     |> Index.to_operations()
     |> wrap_into_delta()
   end
 
-  defp do_compose(:noop, ops_index), do: ops_index
-  defp do_compose(new_op, ops_index) do
-    existing_op = Enum.at(ops_index, Operation.index(new_op), :noop)
-    do_compose(existing_op, new_op, ops_index)
+  defp compose_op_with_index(:noop, ops_index), do: ops_index
+  defp compose_op_with_index(new_op, ops_index) do
+    ops_index
+    |> Enum.at(Operation.index(new_op), :noop)
+    |> compose_op(new_op, ops_index)
   end
 
-  defp do_compose(%{insert: _},
+  defp compose_op(%{insert: _},
                   %{remove: idx}, ops_index) do
     Index.delete_at(ops_index, idx)
   end
-  defp do_compose(%{insert: idx},
+  defp compose_op(%{insert: idx},
                   %{replace: _, init: init}, ops_index) do
     Index.replace_at(ops_index, idx, Operation.insert(idx, init))
   end
-  defp do_compose(%{insert: idx, init: init},
+  defp compose_op(%{insert: idx, init: init},
                   %{change: _, delta: delta}, ops_index) do
     new_init = ItemDelta.compose(init, delta)
     Index.replace_at(ops_index, idx, Operation.insert(idx, new_init))
   end
 
-  defp do_compose(%{remove: idx},
+  defp compose_op(%{remove: idx},
                   %{insert: _, init: init}, ops_index) do
     Index.replace_at(ops_index, idx, Operation.replace(idx, init))
   end
-  defp do_compose(%{remove: _},
+  defp compose_op(%{remove: _},
                   %{change: _}, ops_index) do
+    # this should never happen,
+    # so when it does - we don't care about the op
     ops_index
   end
 
-  defp do_compose(%{replace: idx, init: init},
+  defp compose_op(%{replace: idx, init: init},
                   %{change: _, delta: delta}, ops_index) do
     new_init = ItemDelta.compose(init, delta)
     Index.replace_at(ops_index, idx, Operation.replace(idx, new_init))
   end
 
-  defp do_compose(%{change: idx, delta: delta_a},
+  defp compose_op(%{change: idx, delta: delta_a},
                   %{change: _, delta: delta_b}, ops_index) do
     new_delta = ItemDelta.compose(delta_a, delta_b)
     Index.replace_at(ops_index, idx, Operation.change(idx, new_delta))
   end
 
-  defp do_compose(_left, right, ops_index) do
+  defp compose_op(_left, right, ops_index) do
     Index.add(ops_index, right)
   end
 
