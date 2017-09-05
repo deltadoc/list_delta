01cfbfe4b37ce7ce7a8cc807aefc90145e8cc9b2
everzet <ever.zet@gmail.com>
2017-09-05 20:52:07 +0100

    Add ListDelta.new()

--- lib/list_delta.ex
+++ lib/list_delta.ex
@@ -4,26 +4,29 @@ defmodule ListDelta do
   alias ListDelta.{Operation, Composition}
 
   def new, do: %ListDelta{}
+  def new(ops), do: List.foldl(ops, new(), &append(&2, &1))
 
   def insert(delta \\ %ListDelta{}, idx, init) do
-    compose(delta, wrap(Operation.insert(idx, init)))
+    append(delta, Operation.insert(idx, init))
   end
 
   def remove(delta \\ %ListDelta{}, idx) do
-    compose(delta, wrap(Operation.remove(idx)))
+    append(delta, Operation.remove(idx))
   end
 
   def replace(delta \\ %ListDelta{}, idx, new_init) do
-    compose(delta, wrap(Operation.replace(idx, new_init)))
+    append(delta, Operation.replace(idx, new_init))
   end
 
   def change(delta \\ %ListDelta{}, idx, item_delta) do
-    compose(delta, wrap(Operation.change(idx, item_delta)))
+    append(delta, Operation.change(idx, item_delta))
   end
 
   def operations(delta), do: delta.ops
 
   defdelegate compose(first, second), to: Composition
+
+  defp append(delta, op), do: compose(delta, wrap(op))
 
   defp wrap(ops), do: %ListDelta{ops: List.wrap(ops)}
 end
