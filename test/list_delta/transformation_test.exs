283468e8b589be53cf6e116f66260170a92d5da8
everzet <ever.zet@gmail.com>
2017-09-14 22:42:05 +0100

    Group tests better

--- test/list_delta/transformation_test.exs
+++ test/list_delta/transformation_test.exs
@@ -7,8 +7,8 @@ defmodule ListDelta.TransformationTest do
 
   doctest Transformation
 
-  describe "transforming insert" do
-    test "against insert at the same index" do
+  describe "transforming insert against another insert" do
+    test "at the same index" do
       a = ListDelta.insert(0, "A")
       b = ListDelta.insert(0, "B")
       assert xf(a, b, :left) == ListDelta.insert(1, "B")
@@ -17,7 +17,7 @@ defmodule ListDelta.TransformationTest do
       assert xf(b, a, :right) == ListDelta.insert(0, "A")
     end
 
-    test "against insert at a different index" do
+    test "at a different index" do
       a = ListDelta.insert(0, "A")
       b = ListDelta.insert(2, "B")
       assert xf(a, b, :left) == ListDelta.insert(3, "B")
@@ -25,64 +25,70 @@ defmodule ListDelta.TransformationTest do
       assert xf(b, a, :left) == ListDelta.new([move(2, 1), insert(0, "A")])
       assert xf(b, a, :right) == ListDelta.insert(0, "A")
     end
+  end
 
-    test "against remove at the same index" do
+  describe "transforming insert against remove" do
+    test "at the same index" do
       a = ListDelta.remove(0)
       b = ListDelta.insert(0, "B")
       assert xf(a, b, :left) == ListDelta.insert(0, "B")
       assert xf(b, a, :right) == ListDelta.remove(1)
     end
 
-    test "against remove at a lower index" do
+    test "at a lower index" do
       a = ListDelta.remove(1)
       b = ListDelta.insert(2, "B")
       assert xf(a, b, :left) == ListDelta.insert(1, "B")
       assert xf(b, a, :right) == ListDelta.remove(1)
     end
 
-    test "against remove at a higher index" do
+    test "at a higher index" do
       a = ListDelta.remove(2)
       b = ListDelta.insert(1, "B")
       assert xf(a, b, :left) == ListDelta.insert(1, "B")
       assert xf(b, a, :right) == ListDelta.remove(3)
     end
+  end
 
-    test "against replace at the same index" do
+  describe "transforming insert against replace" do
+    test "at the same index" do
       a = ListDelta.replace(0, "A")
       b = ListDelta.insert(0, "B")
       assert xf(a, b, :left) == ListDelta.insert(0, "B")
       assert xf(b, a, :right) == ListDelta.replace(1, "A")
     end
 
-    test "against replace at a lower index" do
+    test "at a lower index" do
       a = ListDelta.replace(1, "A")
       b = ListDelta.insert(2, "B")
       assert xf(a, b, :left) == ListDelta.insert(2, "B")
       assert xf(b, a, :right) == ListDelta.replace(1, "A")
     end
 
-    test "against replace at a higher index" do
+    test "at a higher index" do
       a = ListDelta.replace(2, "A")
       b = ListDelta.insert(1, "B")
       assert xf(a, b, :left) == ListDelta.insert(1, "B")
       assert xf(b, a, :right) == ListDelta.replace(3, "A")
     end
+  end
 
-    test "against move from the same origin index" do
+  describe "transforming insert against move" do
+    test "from the same origin index" do
       a = ListDelta.move(0, 2)
       b = ListDelta.insert(0, "B")
       assert xf(a, b, :left) == ListDelta.insert(0, "B")
       assert xf(b, a, :right) == ListDelta.move(1, 3)
     end
 
-    test "against move to the same destination index" do
+    test "to the same destination index" do
       a = ListDelta.move(2, 0)
       b = ListDelta.insert(0, "B")
       assert xf(a, b, :left) == ListDelta.insert(0, "B")
       assert xf(b, a, :right) == ListDelta.move(3, 1)
     end
 
-    test "against move between elements later" do
+    test "between elements later" do
       a = ListDelta.move(1, 3)
       b = ListDelta.insert(0, "B")
       assert xf(a, b, :left) == ListDelta.insert(0, "B")
