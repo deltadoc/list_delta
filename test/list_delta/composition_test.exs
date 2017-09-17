8e2892c1a655bfd0aa1c779a9640d287c4043de5
everzet <ever.zet@gmail.com>
2017-09-17 15:41:35 +0100

    Remove unnecessary verbalisation

--- test/list_delta/composition_test.exs
+++ test/list_delta/composition_test.exs
@@ -20,38 +20,38 @@ defmodule ListDelta.CompositionTest do
     end
   end
 
-  describe "composing insert" do
-    test "with insert at a different index maintains both" do
+  describe "insert +" do
+    test "insert at a different index maintains both" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.insert(2, false)
       assert comp(fst, snd) == [insert(0, 3), insert(2, false)]
     end
 
-    test "with insert at the same index maintains both" do
+    test "insert at the same index maintains both" do
       fst = ListDelta.insert(0, 2)
       snd = ListDelta.insert(0, nil)
       assert comp(fst, snd) == [insert(0, 2), insert(0, nil)]
     end
 
-    test "with insert at a lower index maintains both" do
+    test "insert at a lower index maintains both" do
       fst = ListDelta.insert(2, 3)
       snd = ListDelta.insert(0, 5)
       assert comp(fst, snd) == [insert(2, 3), insert(0, 5)]
     end
 
-    test "with remove at a different index maintains both" do
+    test "remove at a different index maintains both" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.remove(4)
       assert comp(fst, snd) == [insert(0, 3), remove(4)]
     end
 
-    test "immediately followed by a remove drops both operations" do
+    test "remove a the same index drops both operations" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.remove(0)
       assert comp(fst, snd) == []
     end
 
-    test "with insert and a remove at the same index drops second insert" do
+    test "insert and a remove at the same index drops second insert" do
       fst =
         ListDelta.new()
         |> ListDelta.insert(0, 3)
@@ -60,7 +60,7 @@ defmodule ListDelta.CompositionTest do
       assert comp(fst, snd) == [insert(0, 3)]
     end
 
-    test "with 2 inserts and a remove at the same index drops third insert" do
+    test "2 inserts and a remove at the same index drops third insert" do
       fst =
         ListDelta.new()
         |> ListDelta.insert(0, 3)
@@ -70,7 +70,7 @@ defmodule ListDelta.CompositionTest do
       assert comp(fst, snd) == [insert(0, 3), insert(0, nil)]
     end
 
-    test "with 2 inserts and a remove at a different index keeps all ops" do
+    test "2 inserts and a remove at a different index keeps all ops" do
       fst =
         ListDelta.new()
         |> ListDelta.insert(0, 3)
@@ -85,138 +85,138 @@ defmodule ListDelta.CompositionTest do
       ]
     end
 
-    test "with replace at the same index changes the insert init" do
+    test "replace at the same index changes the insert init" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.replace(0, "text")
       assert comp(fst, snd) == [insert(0, "text")]
     end
 
-    test "with replace at a different index maintains both" do
+    test "replace at a different index maintains both" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.replace(2, "text")
       assert comp(fst, snd) == [insert(0, 3), replace(2, "text")]
     end
 
-    test "with change at the same index updates the insert init" do
+    test "change at the same index updates the insert init" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.change(0, 6)
       assert comp(fst, snd) == [insert(0, 6)]
     end
 
-    test "with change at a different index maintains both" do
+    test "change at a different index maintains both" do
       fst = ListDelta.insert(0, 3)
       snd = ListDelta.change(2, "text")
       assert comp(fst, snd) == [insert(0, 3), change(2, "text")]
     end
   end
 
-  describe "composing remove" do
+  describe "remove +" do
     @fst ListDelta.remove(0)
 
-    test "with insert at the same index changes insert into replace" do
+    test "insert at the same index changes insert into replace" do
       snd = ListDelta.insert(0, "text")
       assert comp(@fst, snd) == [replace(0, "text")]
     end
 
-    test "with insert at a different index maintains both" do
+    test "insert at a different index maintains both" do
       snd = ListDelta.insert(1, "text")
       assert comp(@fst, snd) == [remove(0), insert(1, "text")]
     end
 
-    test "with change maintains both" do
+    test "change maintains both" do
       snd = ListDelta.change(0, "text")
       assert comp(@fst, snd) == [remove(0), change(0, "text")]
     end
 
-    test "with remove at the same index drops second remove" do
+    test "remove at the same index drops second remove" do
       snd = ListDelta.remove(0)
       assert comp(@fst, snd) == [remove(0)]
     end
 
-    test "with remove at a different index maintains both" do
+    test "remove at a different index maintains both" do
       snd = ListDelta.remove(1)
       assert comp(@fst, snd) == [remove(0), remove(1)]
     end
 
-    test "with replace maintains both" do
+    test "replace maintains both" do
       snd = ListDelta.replace(0, 5)
       assert comp(@fst, snd) == [remove(0), replace(0, 5)]
     end
   end
 
-  describe "composing replace" do
+  describe "replace +" do
     @fst ListDelta.replace(0, 123)
 
-    test "with insert maintains both" do
+    test "insert maintains both" do
       snd = ListDelta.insert(0, "text")
       assert comp(@fst, snd) == [replace(0, 123), insert(0, "text")]
     end
 
-    test "with change at the same index updates replace init" do
+    test "change at the same index updates replace init" do
       snd = ListDelta.change(0, "text")
       assert comp(@fst, snd) == [replace(0, "text")]
     end
 
-    test "with change at a different index maintains both" do
+    test "change at a different index maintains both" do
       snd = ListDelta.change(1, "text")
       assert comp(@fst, snd) == [replace(0, 123), change(1, "text")]
     end
 
-    test "with remove at the same index removes replace" do
+    test "remove at the same index removes replace" do
       snd = ListDelta.remove(0)
       assert comp(@fst, snd) == [remove(0)]
     end
 
-    test "with remove at a different index maintains both" do
+    test "remove at a different index maintains both" do
       snd = ListDelta.remove(1)
       assert comp(@fst, snd) == [replace(0, 123), remove(1)]
     end
 
-    test "with replace at the same index removes first replace" do
+    test "replace at the same index removes first replace" do
       snd = ListDelta.replace(0, 5)
       assert comp(@fst, snd) == [replace(0, 5)]
     end
 
-    test "with replace at a different index maintains both" do
+    test "replace at a different index maintains both" do
       snd = ListDelta.replace(1, 5)
       assert comp(@fst, snd) == [replace(0, 123), replace(1, 5)]
     end
   end
 
-  describe "composing change" do
+  describe "change +" do
     @fst ListDelta.change(0, "abc")
 
-    test "with insert maintains both" do
+    test "insert maintains both" do
       snd = ListDelta.insert(0, "text")
       assert comp(@fst, snd) == [change(0, "abc"), insert(0, "text")]
     end
 
-    test "with change at the same index composes changes" do
+    test "change at the same index composes changes" do
       snd = ListDelta.change(0, "text")
       assert comp(@fst, snd) == [change(0, "text")]
     end
 
-    test "with change at a different index maintains both" do
+    test "change at a different index maintains both" do
       snd = ListDelta.change(1, "text")
       assert comp(@fst, snd) == [change(0, "abc"), change(1, "text")]
     end
 
-    test "with remove at the same index drops change" do
+    test "remove at the same index drops change" do
       snd = ListDelta.remove(0)
       assert comp(@fst, snd) == [remove(0)]
     end
 
-    test "with remove at a different index maintains both" do
+    test "remove at a different index maintains both" do
       snd = ListDelta.remove(1)
       assert comp(@fst, snd) == [change(0, "abc"), remove(1)]
     end
 
-    test "with replace at the same index drops change" do
+    test "replace at the same index drops change" do
       snd = ListDelta.replace(0, 5)
       assert comp(@fst, snd) == [replace(0, 5)]
     end
 
-    test "with replace at a different index maintains both" do
+    test "replace at a different index maintains both" do
       snd = ListDelta.replace(1, 5)
       assert comp(@fst, snd) == [change(0, "abc"), replace(1, 5)]
     end
